3c144a1c5e84bbe295a20911009d1fd6483c55f8
Cafer Can Tuncer <cafercantuncer@proton.me>
2025-03-10 22:55:20 +0300

    Tasks: - Create a new activity for adding notes (AddNoteActivity) - Create a layout for this activity (must contain EditTexts) - Add click listener to FloatingActionButton and start AddNoteActivity - Return data to MainActivity when note insertion is complete - Get the data returned in MainActivity and add it to the notes list

---
+++ app/src/main/java/com/example/simplenotes/AddNoteActivity.kt
@@ -0,0 +1,38 @@
+package com.example.simplenotes
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class AddNoteActivity : AppCompatActivity() {
+    private lateinit var fab: FloatingActionButton
+    private lateinit var etTitle: EditText
+    private lateinit var etText: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_note)
+
+        initViews()
+        initListeners()
+    }
+
+    private fun initViews() {
+        fab = findViewById(R.id.fab)
+        etTitle = findViewById(R.id.etTitle)
+        etText = findViewById(R.id.etText)
+    }
+
+    private fun initListeners() {
+        fab.setOnClickListener {
+            Intent().apply {
+                putExtra("title", etTitle.text.toString())
+                putExtra("text", etText.text.toString())
+                setResult(RESULT_OK, this)
+            }
+            finish()
+        }
+    }
+}
--- app/src/main/java/com/example/simplenotes/MainActivity.kt
+++ app/src/main/java/com/example/simplenotes/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.simplenotes
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,17 +13,33 @@ class MainActivity : AppCompatActivity() {
     private lateinit var notesRecyclerView: RecyclerView
     private lateinit var toolbar: Toolbar
     private lateinit var fab: FloatingActionButton
+    private val notes = mutableListOf<Note>()
+
+    private val createNoteFromAddNoteActivity =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val intent = result.data
+                val title = intent?.getStringExtra("title")
+                val text = intent?.getStringExtra("text")
+                if (!title.isNullOrBlank() && !text.isNullOrBlank()) {
+                    val note = Note(title, text)
+                    notes.add(note)
+                    notesRecyclerView.adapter?.notifyItemInserted(notes.lastIndex)
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initViews()
+        initListeners()
     }
 
     private fun initViews() {
         // Notes RecyclerView
-        val notesAdapter = NotesAdapter(createNotes())
+        val notesAdapter = NotesAdapter(notes)
         notesRecyclerView = findViewById(R.id.notesRecyclerView)
         notesRecyclerView.layoutManager = LinearLayoutManager(this)
         notesRecyclerView.adapter = notesAdapter
@@ -34,7 +52,10 @@ class MainActivity : AppCompatActivity() {
         fab = findViewById(R.id.fab)
     }
 
-    private fun createNotes(): List<Note> {
-        return List(25) { Note("Title $it", "Text $it") }
+    private fun initListeners() {
+        fab.setOnClickListener {
+            createNoteFromAddNoteActivity
+                .launch(Intent(this, AddNoteActivity::class.java))
+        }
     }
 }
