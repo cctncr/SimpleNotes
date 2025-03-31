cffdd9f79be95d3bd67e1dbbd9dc896c14ad5356
Cafer Can Tuncer <cafercantuncer@proton.me>
2025-03-31 06:39:14 +0300

    Task 5: Inject Dependencies in Activities

--- app/src/main/java/com/example/simplenotes/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/simplenotes/ui/main/MainActivity.kt
@@ -3,20 +3,19 @@ package com.example.simplenotes.ui.main
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.room.Room
 import com.example.simplenotes.ui.addnote.AddNoteActivity
-import com.example.simplenotes.data.local.database.NoteDatabase
 import com.example.simplenotes.data.local.entity.Note
-import com.example.simplenotes.data.repository.NoteRepository
 import com.example.simplenotes.databinding.ActivityMainBinding
 import com.example.simplenotes.viewmodel.NoteViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private lateinit var viewModel: NoteViewModel
+    private val viewModel by viewModels<NoteViewModel>()
     private lateinit var adapter: NotesAdapter
     private lateinit var binding: ActivityMainBinding
 
@@ -33,24 +32,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setupViewModel()
         initViews()
         initListeners()
         observeViewModel()
-    }
-
-    private fun setupViewModel() {
-        val db = Room
-            .databaseBuilder(applicationContext, NoteDatabase::class.java, "note-database")
-            .fallbackToDestructiveMigration()
-            .build()
-
-        val noteDao = db.noteDao()
-
-        val repository = NoteRepository.getInstance(noteDao)
-
-        val factory = NoteViewModelFactory(repository)
-        viewModel = ViewModelProvider(this, factory)[NoteViewModel::class.java]
     }
 
     private fun observeViewModel() {
