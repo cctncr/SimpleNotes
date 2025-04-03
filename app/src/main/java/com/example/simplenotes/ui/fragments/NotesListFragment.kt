7a94cbb09d3f82b5e0bc02305ca10944b46e3395
Cafer Can Tuncer <cafercantuncer@proton.me>
2025-04-03 09:17:16 +0300

    Task: Create Notes List Fragment

--- app/src/main/java/com/example/simplenotes/ui/fragments/NotesListFragment.kt
+++ app/src/main/java/com/example/simplenotes/ui/fragments/NotesListFragment.kt
@@ -5,17 +5,76 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.simplenotes.R
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.simplenotes.data.local.entity.Note
+import com.example.simplenotes.databinding.FragmentNotesListBinding
+import com.example.simplenotes.ui.main.NoteTouchHelperCallback
+import com.example.simplenotes.ui.main.NotesAdapter
+import com.example.simplenotes.viewmodel.NoteViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class NotesListFragment : Fragment() {
-   override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentNotesListBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: NoteViewModel by viewModels()
+    private lateinit var notesAdapter: NotesAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_notes_list, container, false)
+    ): View {
+        _binding = FragmentNotesListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupRecyclerView()
+        setupListeners()
+        observeViewModel()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun observeViewModel() {
+        viewModel.allNotes.observe(viewLifecycleOwner) { notes ->
+            notesAdapter.updateNotes(notes)
+        }
+    }
+
+    private fun setupListeners() {
+        binding.fab.setOnClickListener {
+            val action = NotesListFragmentDirections.actionNotesListFragmentToAddNoteFragment()
+            findNavController().navigate(action)
+        }
+    }
+
+    private fun setupRecyclerView() {
+        notesAdapter = NotesAdapter(
+            emptyList<Note>().toMutableList(),
+            onNoteDeleted = { deletedNote ->
+                viewModel.deleteNote(deletedNote)
+            },
+            onNotesReordered = { reorderedNotes ->
+                viewModel.reorderNotes(reorderedNotes)
+            }
+        )
+
+        binding.notesRecyclerView.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = notesAdapter
+
+            val callback = NoteTouchHelperCallback(notesAdapter)
+            val touchHelper = ItemTouchHelper(callback)
+            touchHelper.attachToRecyclerView(this)
+        }
     }
 }
